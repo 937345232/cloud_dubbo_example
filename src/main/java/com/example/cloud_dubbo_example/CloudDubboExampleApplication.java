3d9b1f03696b8c2d84794fbb64f59cd58e70e9af
937345232 <0307434x>
2019-08-30 16:52:24 +0800

    first commit

---
+++ src/main/java/com/example/cloud_dubbo_example/CloudDubboExampleApplication.java
@@ -0,0 +1,13 @@
+package com.example.cloud_dubbo_example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloudDubboExampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CloudDubboExampleApplication.class, args);
+    }
+
+}
